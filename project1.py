cf2a44e8d164fef07b7dfdff0f4a773aa593d975
Logavatsan <logavatsan2003@gmail.com>
2026-03-16 12:41:30 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -5,9 +5,11 @@ print("Loading dataset...")
 
 df = pd.read_csv("earthquake_cleaned.csv")
 
+# fix NaN values
+df = df.where(pd.notnull(df), None)
+
 print("Total rows:", len(df))
 
-# remove duplicate earthquake ids
 df = df.drop_duplicates(subset="id")
 
 print("Rows after removing duplicates:", len(df))
@@ -23,14 +25,14 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
-print("Inserting data into database...")
+print("Inserting data...")
 
 query = """
 INSERT INTO earthquakes
-(id, time, updated, latitude, longitude, depth_km, mag, magType,
-alert, felt, cdi, mmi, code, place, status, tsunami, sig,
-net, nst, dmin, rms, gap, types, ids, sources, type,
-year, month, day, depth_category, strong_earthquake)
+(id,time,updated,latitude,longitude,depth_km,mag,magType,
+alert,felt,cdi,mmi,code,place,status,tsunami,sig,
+net,nst,dmin,rms,gap,types,ids,sources,type,
+year,month,day,depth_category,strong_earthquake)
 VALUES (%s,%s,%s,%s,%s,%s,%s,%s,
 %s,%s,%s,%s,%s,%s,%s,%s,%s,
 %s,%s,%s,%s,%s,%s,%s,%s,%s,
@@ -39,45 +41,13 @@ VALUES (%s,%s,%s,%s,%s,%s,%s,%s,
 
 for i, row in df.iterrows():
 
-    values = (
-        row["id"],
-        row["time"],
-        row["updated"],
-        row["latitude"],
-        row["longitude"],
-        row["depth_km"],
-        row["mag"],
-        row["magType"],
-        row["alert"],
-        row["felt"],
-        row["cdi"],
-        row["mmi"],
-        row["code"],
-        row["place"],
-        row["status"],
-        row["tsunami"],
-        row["sig"],
-        row["net"],
-        row["nst"],
-        row["dmin"],
-        row["rms"],
-        row["gap"],
-        row["types"],
-        row["ids"],
-        row["sources"],
-        row["type"],
-        row["year"],
-        row["month"],
-        row["day"],
-        row["depth_category"],
-        row["strong_earthquake"]
-    )
+    values = tuple(row)
 
     cursor.execute(query, values)
 
 conn.commit()
 
-print("Data inserted successfully!")
+print("Data inserted successfully")
 
 cursor.close()
 conn.close()
