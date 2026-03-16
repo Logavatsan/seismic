df5f175ced79e34c3db5ceff135cf71cd67f6f76
Logavatsan <logavatsan2003@gmail.com>
2026-03-16 12:08:05 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+import mysql.connector
 
 print("Loading dataset...")
 
@@ -14,18 +14,70 @@ print("Rows after removing duplicates:", len(df))
 
 print("Connecting to MySQL...")
 
-engine = create_engine(
-    "mysql+pymysql://root:Loga2003%40@localhost/b115_b118"
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Loga2003@",
+    database="b115_b118"
 )
 
-print("Inserting data...")
+cursor = conn.cursor()
 
-df.to_sql(
-    "earthquakes",
-    engine,
-    if_exists="replace",
-    index=False,
-    chunksize=1000
-)
+print("Inserting data into database...")
+
+query = """
+INSERT INTO earthquakes
+(id, time, updated, latitude, longitude, depth_km, mag, magType,
+alert, felt, cdi, mmi, code, place, status, tsunami, sig,
+net, nst, dmin, rms, gap, types, ids, sources, type,
+year, month, day, depth_category, strong_earthquake)
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s,
+%s,%s,%s,%s,%s,%s,%s,%s,%s,
+%s,%s,%s,%s,%s,%s,%s,%s,%s,
+%s,%s,%s,%s,%s)
+"""
+
+for i, row in df.iterrows():
+
+    values = (
+        row["id"],
+        row["time"],
+        row["updated"],
+        row["latitude"],
+        row["longitude"],
+        row["depth_km"],
+        row["mag"],
+        row["magType"],
+        row["alert"],
+        row["felt"],
+        row["cdi"],
+        row["mmi"],
+        row["code"],
+        row["place"],
+        row["status"],
+        row["tsunami"],
+        row["sig"],
+        row["net"],
+        row["nst"],
+        row["dmin"],
+        row["rms"],
+        row["gap"],
+        row["types"],
+        row["ids"],
+        row["sources"],
+        row["type"],
+        row["year"],
+        row["month"],
+        row["day"],
+        row["depth_category"],
+        row["strong_earthquake"]
+    )
+
+    cursor.execute(query, values)
+
+conn.commit()
 
 print("Data inserted successfully!")
+
+cursor.close()
+conn.close()
